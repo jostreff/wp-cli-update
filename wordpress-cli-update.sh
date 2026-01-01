7a47f874e2eee0dee24ac6c037098e9c98c65978
Jordan Ostreff <j@ostreff.info>
2026-01-01 15:11:42 +0200

    Update WP-CLI update script for error handling

--- wordpress-cli-update.sh
+++ wordpress-cli-update.sh
@@ -1,5 +1,21 @@
 #!/bin/csh
-#/usr/local/bin/wp cli update --allow-root --yes
+/usr/local/bin/wp cli check-update --allow-root --patch --quiet
+# bash version bellow:
+#if [ $? -eq 1 ]; then
+#    echo "Има налична нова версия на WP-CLI!"
+#   /usr/local/bin/wp cli update --allow-root --yes --nightly
+#else
+#    echo "WP-CLI е актуален."
+#fi
+
+# csh version bellow
+if ( $status == 1 )  then
+    echo "Има налична нова версия на WP-CLI!"
+   /usr/local/bin/wp cli update --allow-root --yes --nightly
+else
+    echo "WP-CLI е актуален."
+endif
+
 sudo -u nobody -g nobody -- /usr/local/bin/wp --path=/usr/local/www/ostreff.info/ core update
 sudo -u nobody -g nobody -- /usr/local/bin/wp --path=/usr/local/www/ostreff.info/ plugin update --all
 sudo -u nobody -g nobody -- /usr/local/bin/wp --path=/usr/local/www/ostreff.info/ theme update --all
