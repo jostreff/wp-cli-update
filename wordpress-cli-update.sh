766c5e78eac4ee16409ff867d99f9484ad6f1f48
Jordan Ostreff <j@ostreff.info>
2026-01-18 12:36:58 +0200

    Enhance WP-CLI update script for multiple sites
    Refactor script to handle multiple sites and update operations. Just add paths inside sites array.

--- wordpress-cli-update.sh
+++ wordpress-cli-update.sh
@@ -1,13 +1,5 @@
 #!/bin/csh
 /usr/local/bin/wp cli check-update --allow-root --patch --quiet
-# bash version bellow:
-#if [ $? -eq 1 ]; then
-#    echo "Има налична нова версия на WP-CLI!"
-#   /usr/local/bin/wp cli update --allow-root --yes --nightly
-#else
-#    echo "WP-CLI е актуален."
-#fi
-
 # csh version bellow
 if ( $status == 1 )  then
     echo "Има налична нова версия на WP-CLI!"
@@ -16,9 +8,34 @@ else
     echo "WP-CLI е актуален."
 endif
 
-sudo -u nobody -g nobody -- /usr/local/bin/wp --path=/usr/local/www/ostreff.info/ core update
-sudo -u nobody -g nobody -- /usr/local/bin/wp --path=/usr/local/www/ostreff.info/ plugin update --all
-sudo -u nobody -g nobody -- /usr/local/bin/wp --path=/usr/local/www/ostreff.info/ theme update --all
-sudo -u nobody -g nobody -- /usr/local/bin/wp --path=/usr/local/www/ostreff.info/ language core update
-sudo -u nobody -g nobody -- /usr/local/bin/wp --path=/usr/local/www/ostreff.info/ language plugin update --all
-sudo -u nobody -g nobody -- /usr/local/bin/wp --path=/usr/local/www/ostreff.info/ language theme update --all
+set sites = ( \
+    "/usr/local/www/ostreff.info/" \
+)
+set wp_cli = "sudo -u nobody -g nobody -- /usr/local/bin/wp"
+
+foreach site ($sites)
+    if ( -d "$site" ) then
+        set site_url = `$wp_cli option get home --path=$site`
+        echo "--- Updating site: $site_url ---"
+
+        set update_output = `$wp_cli core update --path="$site"`
+        echo "$update_output"
+        echo "$update_output" | grep -q "is up to date"
+        if ( $status != 0 ) then
+            echo "Core updated, running database update..."
+            $wp_cli core update-db --path="$site"
+        endif
+
+        $wp_cli plugin update --all --path="$site"
+        $wp_cli theme update --all --path="$site"
+
+        $wp_cli language core update --path="$site"
+        $wp_cli language plugin update --all --path="$site"
+        $wp_cli language theme update --all --path="$site"
+
+        echo "Done with $site_url"
+        echo ""
+    else
+        echo "Directory $site not found. Skipping..."
+    endif
+end
