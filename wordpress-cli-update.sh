d1a511844f44889f2a674537d1d69ee80bf87676
Jordan Ostreff <j@ostreff.info>
2026-07-11 21:52:26 +0300

    Update wordpress-cli-update.sh
    embedded ipv4 only wp-cli inside the script

--- wordpress-cli-update.sh
+++ wordpress-cli-update.sh
@@ -10,16 +10,39 @@ else
     echo "WP-CLI is up to date."
 endif
 set sites = ( \
-    "/usr/local/www/ostreff.info/" 
+    "/usr/local/www/ostreff.info/" \
+    "/home/realesr/" \
+    "/home/classic-bg.net/new-site/" \
+    "/home/classic-bg.net/tmp-site/" \
+    "/usr/local/www/wordpress/" \
+    "/usr/local/www/albena-bg.be/" \
 )
-
 set wp_cli = "sudo -u nobody -g nobody WP_CLI_CACHE_DIR=$WP_CLI_CACHE_DIR -- /usr/local/bin/wp"
-set wp_req = "--require=/usr/local/bin/force_wp_use_ipv4.php"
+set wp_req_file = "/tmp/home-nobody/force_wp_use_ipv4.php"
 
-if ( ! -f /usr/local/bin/force_wp_use_ipv4.php ) then
-    echo "WARNING: force_wp_use_ipv4.php not found, continuing without it"
-    set wp_req = ""
+if ( ! -f "$wp_req_file" ) then
+    echo "Generating force_wp_use_ipv4.php in temporary directory..."
+    echo '<?php' > "$wp_req_file"
+    echo '// force_wp_use_ipv4.php - Forces IPv4 for all HTTP requests in WP-CLI' >> "$wp_req_file"
+    echo '// Usage: wp --require=/usr/local/bin/force_wp_use_ipv4.php <command>' >> "$wp_req_file"
+    echo '' >> "$wp_req_file"
+    echo 'if (defined("WP_CLI") && WP_CLI) {' >> "$wp_req_file"
+    echo '    // Hook AFTER WordPress is loaded, so add_action/add_filter exist' >> "$wp_req_file"
+    echo '    WP_CLI::add_hook("after_wp_load", function() {' >> "$wp_req_file"
+    echo '        // Force IPv4 for WordPress HTTP API' >> "$wp_req_file"
+    echo '        add_action("http_api_curl", function($handle) {' >> "$wp_req_file"
+    echo '            curl_setopt($handle, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);' >> "$wp_req_file"
+    echo '        }, PHP_INT_MIN, 1);' >> "$wp_req_file"
+    echo '' >> "$wp_req_file"
+    echo '        // Disable SSL verification (for testing)' >> "$wp_req_file"
+    echo '        // add_filter("https_ssl_verify", "__return_false");' >> "$wp_req_file"
+    echo '        // add_filter("https_local_ssl_verify", "__return_false");' >> "$wp_req_file"
+    echo '    });' >> "$wp_req_file"
+    echo '}' >> "$wp_req_file"
+    chown nobody:nobody "$wp_req_file"
+    chmod 644 "$wp_req_file"
 endif
+set wp_req = "--require=$wp_req_file"
 
 foreach site ($sites)
     if ( -d "$site" ) then
